b6d888d5e1c3b770719b4743dd8f3336fe1779c8
鲍子轩 <bzx0574@126.com>
2020-11-30 10:07:57 +0800

    first commit

---
+++ newDemo/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace newDemo.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
